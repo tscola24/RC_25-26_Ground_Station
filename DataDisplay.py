e3b4dc3e3184d79fadbd3325d727cf4854558991
tscola24 <theoscola@gmail.com>
2026-01-14 16:52:37 -0500

    Added live data functionality

--- DataDisplay.py
+++ DataDisplay.py
@@ -1,42 +1,41 @@
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 import pandas as pd
-import random 
+from pathlib import Path
+
+csv_file_path_name = Path(Path.cwd(), 'data', 'data.csv')
 
 measures = ['air_pressure', 'altitude','acceleration','payload_temp','rotation_rate', 'descent_rate']
-units = ['psi', 'ft', 'ft/s sqaured','degrees', 'rotations per second', 'ft/s']
-random_ints = []
-for i in range(0,18):
-    random_ints.append(random.randint(0,100))
-
-data_dict = {
-    'air_pressure' : random_ints[0:3],
-    'altitude' : random_ints[3:6],
-    'acceleration' : random_ints[6:9],
-    'payload_temp' : random_ints[9:12],
-    'rotation_rate' : random_ints[12:15],
-    'descent_rate' : random_ints[15:18],
-    'time' : [0,1,2]
-}
-
-data = pd.DataFrame(data_dict)
-
+units = ['Hgin', 'ft', 'ft/s sqaured','degrees', 'rotations per second', 'ft/s']
 n_rows = 2
 n_columns = round((len(measures)/n_rows))
 
 fig, axes = plt.subplots(nrows=n_rows, ncols=n_columns, figsize=(12,8))
 fig.set_facecolor("#AAC2E7")
 
-for i, measure in enumerate(measures):
-    row = i // (n_rows+1)
-    col = i % n_columns
+def updateDisplay(frame_number):
+    try:
+        data = pd.read_csv(csv_file_path_name)
+    except Exception:
+        return 
+    if data.empty:
+        return
+    
+    for i, measure in enumerate(measures):
+        row = i // n_columns
+        col = i % n_columns
 
-    ax = axes[row][col]
-    ax.plot(data['time'], data[measure], color='red', linewidth=2)
+        ax = axes[row][col]
+        ax.cla()
+        ax.plot(data['time'], data[measure], color='red', linewidth=2)
 
-    ax.set_title(f'{measure}')
-    ax.set_ylabel(f'{units[i]}')
-    ax.set_xlabel('Time (seconds)')
-    ax.title.set_color('black')
+        ax.set_title(f'{measure}')
+        ax.set_ylabel(f'{units[i]}')
+        ax.set_xlabel('Time (seconds)')
+        
+    plt.tight_layout()
 
-plt.tight_layout()
+
+updatedDisplay = FuncAnimation(fig, updateDisplay, interval=500, cache_frame_data=False)
+
 plt.show()
---
+++ data_to_csv.py
@@ -0,0 +1,60 @@
+import csv
+import random
+import time
+from pathlib import Path
+import sys
+
+csv_file_path_name = Path(Path.cwd(), 'data', 'data.csv')
+
+print(f"Current Working Directory: {Path.cwd()}")
+print(f"Target File Path: {csv_file_path_name}")
+
+try: 
+    csv_file_path_name.parent.mkdir(parents=True, exist_ok=True)
+except Exception as err:
+    print(err)
+    sys.exit()
+
+header_column_names = ['time','air_pressure', 'altitude','acceleration','payload_temp','rotation_rate', 'descent_rate']
+time_ = 0
+air_pressure = 30
+altitude = 500
+acceleration = 20
+payload_temp = 20
+rotation_rate = 6
+descent_rate = -10
+
+
+
+with open(csv_file_path_name, 'w') as csv_file:
+    csv_writer = csv.DictWriter(csv_file, fieldnames=header_column_names)
+    csv_writer.writeheader()
+
+while True:
+
+    with open(csv_file_path_name, 'a') as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames=header_column_names)
+
+        info = {
+            'time' : time_,
+            'air_pressure' : air_pressure,
+            'altitude' : altitude,
+            'acceleration' : acceleration,
+            'payload_temp' : payload_temp,
+            'rotation_rate' : rotation_rate,
+            'descent_rate' : descent_rate
+        }
+
+        csv_writer.writerow(info)
+        print(time_,air_pressure, altitude,acceleration,payload_temp,rotation_rate, descent_rate)
+
+        
+        time_ += 0.5
+        air_pressure += random.randint(-1, 1)
+        altitude += random.randint(3, 9)
+        acceleration += random.randint(-3, 3)
+        payload_temp += random.randint(-1, 1)
+        rotation_rate += random.randint(-1, 1)
+        descent_rate += random.randint(-3, 3)
+
+    time.sleep(0.5)
